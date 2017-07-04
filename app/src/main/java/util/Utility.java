20b864e3461288ea99c05628221cb46959d2b9bb
anyue <anyue@mail.com>
2017-07-04 17:36:00 +0800

    加入显示天气信息功能

--- app/src/main/java/util/Utility.java
+++ app/src/main/java/util/Utility.java
@@ -3,6 +3,8 @@ package util;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.google.gson.Gson;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -10,6 +12,7 @@ import org.json.JSONObject;
 import db.City;
 import db.County;
 import db.Province;
+import gson.Weather;
 
 /**
  * Created by Administrator on 2017/7/3.
@@ -83,4 +86,20 @@ public class Utility {
         }
         return false;
     }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+            return null;
+    }
+
 }
